54e6005d8e6d687a691fd99ee4d248f14c3cc9f8
developfor <will@developfor.com>
2015-08-29 13:29:02 -0400

    adding temp data to db and pulling data from db with route.

--- routes/routes.js
+++ routes/routes.js
@@ -3,17 +3,27 @@
 var mongoose = require('mongoose');
 var Profile = require('../models/profile.js');
 
-Profile.find({}, null, function(err, profiles){
-	console.log("err: " + err);
-	console.log(profiles);
+var profile = new Profile({ first_name: 'fluffy_b', 
+							last_name: 'cat_b', 
+							description: 'This is a stupid fluffy cat.'
+						});
 
-})
+profile.save(function (err, profile) {
+  if (err) return console.error(err);
+});
+
+
 
 module.exports = function(app) {
 
-	// app.get('/', function (req, res) {
+	app.get('/', function (req, res) {
+		return Profile.find({}, null, function(err, profiles){
+			console.log("err: " + err);
+			console.log(profiles);
+			res.send(profiles)
+		})
 
-	// 	res.send('hi')
+		
 	// 	// Profile.find({}).exec(function(err, profile){
 	// 	// 	console.log(profile)
 	// 	// })
@@ -33,7 +43,7 @@ module.exports = function(app) {
 	// //  //      }
 	// //  //    });
  // //  		// res.send('one two three');
-	// });
+	});
 
 	// // app.get('/profiles', function (req, res) {
  // //  		// res.send('this is');
